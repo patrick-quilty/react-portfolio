aa27909c13ffbe845159ec0fb8da4464492c5038
Pat Quilty <p.quilty@yahoo.com>
2019-12-12 02:26:52 -0500

    Updates

---
+++ precache-manifest.de6201e3f10eb87ea7fa1045253767ed.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcef0142e7de47ba9fd55b0cea5ee72d",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "a7f39dd161d40aef24de",
+    "url": "/react-portfolio/static/css/main.a9bae9be.chunk.css"
+  },
+  {
+    "revision": "e88c12304c4b8dc1e95d",
+    "url": "/react-portfolio/static/js/2.b3f5e6ba.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-portfolio/static/js/2.b3f5e6ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a7f39dd161d40aef24de",
+    "url": "/react-portfolio/static/js/main.49ff2663.chunk.js"
+  },
+  {
+    "revision": "2f4127c43447fa26b9dd",
+    "url": "/react-portfolio/static/js/runtime-main.609804ff.js"
+  },
+  {
+    "revision": "39b5b83f47eb37ec3040cfa3a999bb22",
+    "url": "/react-portfolio/static/media/SMBC20110908.39b5b83f.png"
+  },
+  {
+    "revision": "a949c0d6d91143aa9e5193fa5b177fc9",
+    "url": "/react-portfolio/static/media/XKCD664.a949c0d6.png"
+  },
+  {
+    "revision": "1b263a7f40db499a48e27c88c4ef6d8c",
+    "url": "/react-portfolio/static/media/headshot.1b263a7f.jpg"
+  },
+  {
+    "revision": "df2215ab8c9a26721be341d4bbb199c4",
+    "url": "/react-portfolio/static/media/icon-CSS3.df2215ab.png"
+  },
+  {
+    "revision": "2915f86f38fc4e34f657e1ac3e76cfb8",
+    "url": "/react-portfolio/static/media/icon-email.2915f86f.png"
+  },
+  {
+    "revision": "3420e571b3d7a4a348d8fad91e3bfda4",
+    "url": "/react-portfolio/static/media/icon-jQuery.3420e571.png"
+  },
+  {
+    "revision": "d37661928b07f40d7154547d33d40779",
+    "url": "/react-portfolio/static/media/icon-node.d3766192.png"
+  },
+  {
+    "revision": "37beda9d8a3eea99f777b4d2145f1fc5",
+    "url": "/react-portfolio/static/media/icon-phone.37beda9d.png"
+  },
+  {
+    "revision": "5f9fcdd95528d49afb66756a3218649a",
+    "url": "/react-portfolio/static/media/icon-react.5f9fcdd9.png"
+  },
+  {
+    "revision": "9cb54203eef0076836e59f656bcf52c0",
+    "url": "/react-portfolio/static/media/spouts-screen.9cb54203.png"
+  },
+  {
+    "revision": "551252452566ee1240dcd4471fdb12a1",
+    "url": "/react-portfolio/static/media/sudoku-screen.55125245.png"
+  },
+  {
+    "revision": "85f8510f709de142bc04a64078209112",
+    "url": "/react-portfolio/static/media/yoga-screen.85f8510f.png"
+  }
+]);
